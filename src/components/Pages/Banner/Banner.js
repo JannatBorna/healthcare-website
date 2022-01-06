72bd54211ec820cb0d2dedc187c7f49711922c2f
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-01-06 23:43:27 +0600

    footer

--- src/components/Pages/Banner/Banner.js
+++ src/components/Pages/Banner/Banner.js
@@ -9,15 +9,15 @@ const Banner = () => {
         <Container className="mt-2">
 
             <div className="container_banner my-3">
-                <div className="container_header">
-                    <h1><span className="color_text">Health care</span><span className="health_care">  lack of health may negatively affect health.</span></h1>
+                <div className="container_header mt-5">
+                    <h1><span className="color_text ">Health care</span><span className="health_care">  lack of health may negatively affect health.</span></h1>
                     <p className="p_tag">Your Health Is Your Priority. But I must explain to you how all this mistaken idea of denouncing.</p>
 
                     <Link to="/about" className="button_color"><Button>Read More</Button></Link>
                 </div>
 
                 <div className="picture">
-                    <img className="w-100" src={slide2} alt="" rounded/>
+                    <img className="w-100 " src={slide2} alt="" rounded/>
                 </div>
                 
             </div>
